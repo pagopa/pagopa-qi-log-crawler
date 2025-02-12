e004758e2882d1ea4deff35f76c7d523032502d5
Simone Esposito <simone.esposito1986@gmail.com>
2025-02-12 14:47:58 +0100

    add activateIOPayment

--- src/payload/req/activateIOPayment.php
+++ src/payload/req/activateIOPayment.php
@@ -6,6 +6,13 @@ use pagopa\sert\payload\RequestXML;
 
 class activateIOPayment extends RequestXML
 {
+    const string XPATH_NAV              = '//activateIOPaymentReq/qrCode/noticeNumber';
+    const string XPATH_PA_EMITTENTE     = '//activateIOPaymentReq/qrCode/fiscalCode';
+    const string XPATH_PSP_ID           = '//activateIOPaymentReq/idPSP';
+    const string XPATH_BROKER_PSP       = '//activateIOPaymentReq/idBrokerPSP';
+    const string XPATH_BROKER_CHANNEL   = '//activateIOPaymentReq/idChannel';
+    const string XPATH_AMOUNT           = '//activateIOPaymentReq/amount';
+    const string XPATH_TOTAL_AMOUNT     = '//activateIOPaymentReq/amount';
 	public function getPaymentsCount(): int
 	{
 		return 1;
